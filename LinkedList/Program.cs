bddc3cddcc4da364b2d1cf351ba00be2aad65587
Aftab-1998 <alam41976@gmail.com>
2021-12-12 23:39:05 +0530

    [Aftab2] Add ; Added the adding operation

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -6,10 +6,11 @@ namespace DataStructure
     {
         static void Main(string[] arg)
         {
+            
             LinkedList LinkedList = new LinkedList();
-            LinkedList.Add(56);
-            LinkedList.Add(30);
-            LinkedList.Add(70);
+            LinkedList.AddReverse(70);
+            LinkedList.AddReverse(30);
+            LinkedList.AddReverse(56);
             LinkedList.Display();
             Console.WriteLine("------");
         }
