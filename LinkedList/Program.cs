073d74e3634552aff9a778f3f25fe05ddd2a3c91
Aftab-1998 <alam41976@gmail.com>
2021-12-12 23:07:31 +0530

    [Aftab1] Add ; Added the number of linked list

---
+++ LinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+namespace DataStructure
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            LinkedList LinkedList = new LinkedList();
+            LinkedList.Add(56);
+            LinkedList.Add(30);
+            LinkedList.Add(70);
+            LinkedList.Display();
+            Console.WriteLine("------");
+        }
+    }
+
+}
